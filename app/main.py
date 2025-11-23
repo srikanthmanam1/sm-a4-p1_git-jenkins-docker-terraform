c8f39bbfe57285caf364333d41619a1a8c094c79
S M <msrikanth.utd@gmail.com>
2025-11-23 13:26:15 -0600

    Updated Testing Python file

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from flask import Flask
+import time
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Jenkins + Docker + Terraform + AWS!"
+	print("No 1 Printed immediately.")
+	time.sleep(5) # Suspends execution for 5 seconds
+	print("No 1 Printed after 5 seconds.")
+    	return "Hello from Jenkins + Docker + Terraform + AWS!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    	app.run(host="0.0.0.0", port=8080)
+        #print("No 2 Printed immediately.")
+        #time.sleep(30) # Suspends execution for 30 seconds
+        #print("No 2 Printed after 30 seconds.")
 
