e23e73f663f4874e94aa806d22dc8441bd51a91b
srikanthmanam1 <166566889+srikanthmanam1@users.noreply.github.com>
2025-11-23 21:33:11 -0600

    Updated main.tf commented aws_route

--- terraform/main.tf
+++ terraform/main.tf
@@ -76,11 +76,11 @@ resource "aws_route_table" "rtable" {
 #  gateway_id             = aws_internet_gateway.igw.id
 #}
 
-resource "aws_route" "internet_access" {
-  route_table_id         = aws_route_table.rtable.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
-}
+#resource "aws_route" "internet_access" {
+#  route_table_id         = aws_route_table.rtable.id
+#  destination_cidr_block = "0.0.0.0/0"
+#  gateway_id             = aws_internet_gateway.igw.id
+#}
 
 # Associate subnet with route table
 resource "aws_route_table_association" "assoc" {
