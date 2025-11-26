06ceb282cef9f88c78d53308eec3f284288bfc24
srikanthmanam1 <166566889+srikanthmanam1@users.noreply.github.com>
2025-11-25 20:49:59 -0600

    Update providers.tf Info on role, IMDS

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,3 +9,15 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+# Ensure your EC2 instance has an IAM role attached with the necessary permissions
+# Check that IMDS is enabled on the instance. You can verify this in the EC2 console or using AWS CLI:
+
+# In EC2 Settings 
+# aws ec2 describe-instances --instance-ids i-0bc36a327f31838fd
+# IMDS defaults Info -- Manage
+# Set the IMDS defaults at the account level for new instance launches in this Region.
+# Instance metadata service Enabled
+# Metadata version No preference
+# Access to tags in metadata Enabled
+# Metadata response hop limit No preference
